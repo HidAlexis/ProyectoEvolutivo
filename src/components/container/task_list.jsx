f1bc7a094de8041c5093e3da145d9adf3bc00a6d
Hidalgo Alexis Alejandro <alexishidale@gmail.com>
2023-02-13 00:06:32 -0300

    Creo rama y empiezo con el proyecto evolutivo

---
+++ src/components/container/task_list.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { LEVELS } from '../../models/levels.enum';
+import { Task } from '../../models/task.class'
+import TaskComponent from '../pure/task';
+
+
+const TaskListComponent = () => {
+
+    const defaultTask = new TaskListComponent('Example', 'Default description', false, LEVELS.NORMAL);
+
+    //Estado del componente
+    const [tasks, setTasks] = useState([defaultTask]);
+
+    //Control del ciclo de vida del componente
+    useEffect(() => {
+        console.log("Task State has been modified")
+        return () => {
+            console.log("TaskLIst component is going to unmount...")
+        };
+    }, [tasks]);
+    
+    const changeCompleted = (id) => {
+        console.log("TODO: Cambiar estao de una tarea")
+    }
+
+    return (
+        <div>
+            <div>
+                <h1>Your Tasks:</h1>
+            </div>
+            {/**Aplicar un For/Map para renderizar una lista */}
+            <TaskComponent task={defaultTask}></TaskComponent>
+        </div>
+    );
+}
+
+export default TaskListComponent;
